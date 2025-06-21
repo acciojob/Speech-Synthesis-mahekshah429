544671a7543faf275d56912b75834121d0a38a71
Mahek shah <149513184+mahekshah429@users.noreply.github.com>
2025-06-21 11:09:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-// Your script here.
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector('#speak');
+const stopButton = document.querySelector('#stop');
+
+function populateVoices() {
+  voices = speechSynthesis.getVoices();
+  voicesDropdown.innerHTML = voices
+    .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+    .join('');
+}
+
+function setVoice() {
+  msg.voice = voices.find(voice => voice.name === voicesDropdown.value);
+  toggle();
+}
+
+function toggle(startOver = true) {
+  speechSynthesis.cancel();
+  if (startOver) {
+    speechSynthesis.speak(msg);
+  }
+}
+
+function setOption() {
+  msg[this.name] = this.value;
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
+options.forEach(option => option.addEventListener('change', setOption));
+speakButton.addEventListener('click', () => toggle(true));
+stopButton.addEventListener('click', () => toggle(false));
+
+// Set default values
+msg.text = document.querySelector('[name="text"]').value;
+msg.rate = document.querySelector('[name="rate"]').value;
+msg.pitch = document.querySelector('[name="pitch"]').value;
